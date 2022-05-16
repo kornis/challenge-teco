df4a89ae10805e396b1c88347791ea956f769319
Federico Garcia <fedegarcia222@gmail.com>
2022-05-16 03:34:18 -0300

    se agrega dist a gitignore

--- dist/js/src/Application/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IpLocationService = void 0;
-var IpLocation_service_1 = require("./IpLocation/IpLocation.service");
-Object.defineProperty(exports, "IpLocationService", { enumerable: true, get: function () { return IpLocation_service_1.IpLocationService; } });
--- dist/js/utils/inversify_identifiers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    "IPLOCATOR": Symbol.for("iplocator"),
-    "IPLOCATORSERVICE": Symbol.for("iplocatorservice")
-};
